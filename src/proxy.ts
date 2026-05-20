79ce810af1dd872c6eb109d67def173841a7e79e
kennedysenyo <kennedysenyo@gmail.com>
2026-05-20 00:44:02 +0000

    add route protection

---
+++ src/proxy.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/lib/better-auth/auth";
+
+const authRoutes = [
+  "/sign-in",
+  "/forgot-password",
+  "/verify-email",
+  "/set-new-passwords",
+];
+
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+
+  const isAuthed = !!session?.session;
+
+  if (authRoutes.includes(pathname) && isAuthed) {
+    return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+  }
+
+  if (!isAuthed && pathname.startsWith("/admin/dashboard")) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
