c1e005f96aa53353ce18417f2e94d0727febda68
kennedysenyo <kennedysenyo@gmail.com>
2026-05-25 07:59:05 +0000

    change drizzl connection config

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,5 +1,14 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { config } from "dotenv";
-config({ path: ".env.local" });
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { config } from "dotenv";
+// config({ path: ".env.local" });
 
-export const db = drizzle(process.env.DATABASE_URL!);
+// export const db = drizzle(process.env.DATABASE_URL!);
+// src/db.ts
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+
+config({ path: ".env" }); // or .env.local
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
